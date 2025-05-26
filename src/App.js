1259e6688dd4034f379bd6dae9636b67fcc1fbc1
WaiYanHein <waiyanhein5796.tgi@gmail.com>
2025-05-26 17:13:57 +0630

    Ch2-Ep9-detail concept of stylesheet in react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import QAImage from './assets/logo.jpg';
+
 import './App.css';
 
 function App() {
@@ -7,17 +6,9 @@ function App() {
   let name="Wai Yan Hein";
 
   return (
-    <div>
+    <div className='app'>
       <h1>Hello {name}</h1>
-
-      {/* 1st way - public folder way */}
-      {/* absolute path */}
-      {/* <img src='http://localhost:3000/logo.jpg'></img> */}
-      {/* relative path */}
-      {/* <img src='/logo.jpg'></img> */}
-
-      {/* 2nd import */}
-      <img src={QAImage}></img>
+        
     </div>
   );
 }
