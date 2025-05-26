79aad2ade497f47b9f2de357a86a06d701c7bcbb
WaiYanHein <waiyanhein5796.tgi@gmail.com>
2025-05-26 16:45:57 +0630

    Ch1-Ep7-Create React App CLI and project structure

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import QAImage from './assets/logo.jpg';
 import './App.css';
 
 function App() {
@@ -6,7 +7,18 @@ function App() {
   let name="Wai Yan Hein";
 
   return (
-    <h1>Hello {name}</h1>
+    <div>
+      <h1>Hello {name}</h1>
+
+      {/* 1st way - public folder way */}
+      {/* absolute path */}
+      {/* <img src='http://localhost:3000/logo.jpg'></img> */}
+      {/* relative path */}
+      {/* <img src='/logo.jpg'></img> */}
+
+      {/* 2nd import */}
+      <img src={QAImage}></img>
+    </div>
   );
 }
 
