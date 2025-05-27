40c6b0b6dbdb1f2413989e9076b5a21164a54797
WaiYanHein <waiyanhein5796.tgi@gmail.com>
2025-05-27 10:07:02 +0630

    Ch3-Ep11-usestate hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
-  let name="Wai Yan Hein";
+  // let name="Wai Yan Hein";
+
+  let [name,setName]=useState('Wai Yan Hein');
+
+  let changename=()=>{
+    // name="Aung AUng";
+    setName('Aung Aung Tun');
+    console.log(name);
+  }
 
   return (
     <div className='app'>
       <h1>Hello {name}</h1>
+      <button onClick={changename}>change name</button>
         
     </div>
   );
