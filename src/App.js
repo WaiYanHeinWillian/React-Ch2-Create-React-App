547c95594d43161e74791d29ab958f119fb42716
WaiYanHein <waiyanhein5796.tgi@gmail.com>
2025-05-29 15:26:11 +0630

    Ch3-Ep13-Working with list

--- src/App.js
+++ src/App.js
@@ -4,12 +4,24 @@ import { useState } from 'react';
 
 function App() {
 
-  // let name="Wai Yan Hein";
-
   let [name,setName]=useState('Wai Yan Hein');
+  let [posts,setPosts]=useState([
+    {
+      id:1,
+      title:'First Post'
+    },
+    {
+      id:2,
+      title:'Second Post'
+    },
+    {
+      id:3,
+      title:'Third Post'
+    }
+  ]);
+
 
   let changename=()=>{
-    // name="Aung AUng";
     setName('Aung Aung Tun');
     console.log(name);
   }
@@ -19,6 +31,13 @@ function App() {
       <h1>Hello {name}</h1>
       <button onClick={changename}>change name</button>
         
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post)=>(
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+
     </div>
   );
 }
