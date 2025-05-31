86cbb191d37eb213d39163f81771439b079d890f
WaiYanHein <waiyanhein5796.tgi@gmail.com>
2025-05-31 11:35:37 +0630

    Ch3-Ep19-react default children prop

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { useState } from 'react';
 import Navbar from './components/Navbar';
 import PostsList from './components/PostsList';
+import Modal from './components/Modal';
 
 function App() {
 
@@ -28,6 +29,11 @@ function App() {
       <Navbar/>
 
       <PostsList posts={posts}/>
+
+      <Modal>
+        <h1>Zoom class is available now</h1>
+        <p>Feel free to <a href=''>join</a></p>
+      </Modal>
       
     </>
   );
