a7ff05d77b90c54afeeecfe4206a8f8dfe385ba5
WaiYanHein <waiyanhein5796.tgi@gmail.com>
2025-06-01 10:32:17 +0630

    Ch4-Ep22-global vs component stylesheet

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,13 +4,15 @@ import './index.css';
 export default function Navbar({setShowModal}) {
   return (
     <nav>
-      <div className='container'>
-        <h1>Logo</h1>
-        <ul>
-          <li>Home</li>
-          <li>Posts</li>
-          <li onClick={()=>setShowModal(true)}>Sign in</li>
-        </ul>
+      <div className='navbar'>
+        <div className='container'>
+          <h1>Logo</h1>
+            <ul>
+              <li>Home</li>
+              <li>Posts</li>
+              <li onClick={()=>setShowModal(true)}>Sign in</li>
+            </ul>
+        </div>
       </div>
     </nav>
   )
