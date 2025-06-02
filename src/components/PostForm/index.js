bba8554a629b3970caf8415ffb214e4dd4fadd5c
WaiYanHein <waiyanhein5796.tgi@gmail.com>
2025-06-02 11:49:11 +0630

    Ch5-Ep26-react 2 way data binding

--- src/components/PostForm/index.js
+++ src/components/PostForm/index.js
@@ -5,15 +5,22 @@ export default function Index() {
 
     let [title,setTitle]=useState("");
 
+    let resetForm=()=>{
+      setTitle('');
+      console.log("update rest");
+    }
+
   return (
     <form className='post-form'>
         <h1>Create Post</h1>
         <div className='form-control'>
             <label htmlFor=''>Title</label>
-            <input type='text' onChange={(e)=>setTitle(e.target.value)}></input>
+            <input type='text' onChange={(e)=>setTitle(e.target.value)} value={title}></input>
         </div>
 
         <p>{title}</p>
+
+        <button type='button' onClick={resetForm}>Rest Form</button>
 
         <div className='form-control'>
             <button>Post Now</button>
